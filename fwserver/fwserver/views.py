9a801da91cea23901746afee72393c9c360c643b
RobertGraves <robert.graves@getkoached.org>
2020-12-28 17:48:48 -0700

    procfile + adjustments + error fix

--- fwserver/fwserver/views.py
+++ fwserver/fwserver/views.py
@@ -1,8 +1,18 @@
 from rest_framework.views import APIView
+from rest_framework.response import Response
 
 
-class HookView(APIView)
-    def get(self,request,*args,**kwargs):
+class HookView(APIView):
+    def post(self,request,*args,**kwargs):
         print(f"request {request}")
+        print(f"request.POST {request.POST}")
         print(f"request.data {request.data}")
-        return Response(data={'data':'goes_here'}status=200)
+
+        typical_data = request.data
+        
+        """
+            format/send hook data to google sheets here
+        """
+
+
+        return Response(status=200)
